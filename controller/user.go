fed188a9e73a97bfa5e2595cc037a8687dfd2cbf
root <root@DESKTOP-75JFS0D.localdomain>
2025-09-20 22:07:03 +0800

    feat: 优雅关闭

--- controller/user.go
+++ controller/user.go
@@ -1,10 +1,14 @@
 package controller
 
 import (
+	"time"
+	
 	"github.com/RZXBxie/web_server/framework"
 )
 
 func UserLoginController(c *framework.Context) error {
-	c.SetOkStatus().Json("ok, UserLoginController")
+	foo, _ := c.QueryString("foo", "def")
+	time.Sleep(10 * time.Second)
+	c.SetOkStatus().Json("ok, UserLoginController" + foo)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
-
+	"os"
+	"os/signal"
+	"syscall"
+	
 	"github.com/RZXBxie/web_server/framework"
 	"github.com/RZXBxie/web_server/middleware"
 )
@@ -16,7 +21,19 @@ func main() {
 		Handler: core,
 		Addr:    ":8080",
 	}
-
-	server.ListenAndServe()
-
+	
+	go func() {
+		server.ListenAndServe()
+	}()
+	
+	// 新建一个Signal类型的channel
+	quit := make(chan os.Signal)
+	// 订阅这三种类型的关闭信号
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-quit
+	
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("shutdown server error: %v", err)
+	}
+	
 }
